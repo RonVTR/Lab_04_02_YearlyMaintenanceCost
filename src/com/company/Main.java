43713995d39637b7d408d2bd10e2bced7cfdb090
aaronvictor <aaron.victor30@gmail.com>
2022-06-05 22:36:46 -0400

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,16 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+        int springCost = 1000;
+        int summerCost = 500;
+        int winterCost = 100;
+        int fallCost = 465;
+
+        System.out.println(springCost + summerCost + winterCost + fallCost + " is your yearly maintenance cost");
+
+
+
+    }
+}
